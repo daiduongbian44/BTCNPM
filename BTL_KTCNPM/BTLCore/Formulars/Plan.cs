693f9936f321c74bec88a28c5f8c713a9a677d2a
manhnguyen <nguyenquangmanh9099@gmail.com>
2015-01-03 21:26:13 +0700

    entity _final

--- BTL_KTCNPM/BTLCore/Formulars/Plan.cs
+++ BTL_KTCNPM/BTLCore/Formulars/Plan.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+
+using BTLCore.Entities;
 
 namespace BTLCore.Formulars
 {
     public class Plan
     {
+        public ObservableCollection<PlanDetailEntity> ListItemPlan;
     }
 }
--- BTL_KTCNPM/BTLCore/Managers/TERelationshipManager.cs
+++ BTL_KTCNPM/BTLCore/Managers/TERelationshipManager.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
+using BTLCore.Entities;
 
 namespace BTLCore.Managers
 {
-    class TERelationshipManager
+    public class TERelationshipManager
     {
+        public static ObservableCollection<TERelationshipEntity> ListTERelationship { get; set; }
+
+        static TERelationshipManager()
+        {
+            ListTERelationship = new ObservableCollection<TERelationshipEntity>();
+        }
     }
 }
